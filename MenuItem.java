2195b48c704262c8c7d80d6c461e59ce28dc64b4
AresUII <42591316+AresUII@users.noreply.github.com>
2020-11-27 14:08:39 -0500

    Add files via upload

---
+++ MenuItem.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurantsystem;
+
+/**
+ *
+ * @author AresUII
+ */
+public class MenuItem {
+    String itemName;
+    //TODO: bools or bytes for any options we need to add
+    public void setName(String a){
+        //Placeholder
+    }
+}
