90540b4515ccb422919a5922e03db1d01a925d82
AresUII <42591316+AresUII@users.noreply.github.com>
2020-11-30 13:37:01 -0500

    Added some messages

--- MealTable.java
+++ MealTable.java
@@ -17,9 +17,36 @@ public class MealTable {
             this.tableStatus=a;
         }
     }
-    byte[] isClean;
-    byte[] isOccupied;
-    byte[] isDirty;
+    byte[] isClean={
+        (byte)('c'&0xff),
+        (byte)('l'&0xff),
+        (byte)('e'&0xff),
+        (byte)('a'&0xff),
+        (byte)('n'&0xff),
+        (byte)(' '&0xff),
+        (byte)(' '&0xff),
+        (byte)(' '&0xff)
+    };
+    byte[] isOccupied={
+        (byte)('o'&0xff),
+        (byte)('c'&0xff),
+        (byte)('c'&0xff),
+        (byte)('u'&0xff),
+        (byte)('p'&0xff),
+        (byte)('i'&0xff),
+        (byte)('e'&0xff),
+        (byte)('d'&0xff)
+    };
+    byte[] isDirty={
+        (byte)('d'&0xff),
+        (byte)('i'&0xff),
+        (byte)('r'&0xff),
+        (byte)('t'&0xff),
+        (byte)('y'&0xff),
+        (byte)(' '&0xff),
+        (byte)(' '&0xff),
+        (byte)(' '&0xff),
+    };
     public void printStatus(){
         if(this.tableStatus==0){ByteCLI.printBytes(isClean);}
         if(this.tableStatus==1){ByteCLI.printBytes(isOccupied);}
