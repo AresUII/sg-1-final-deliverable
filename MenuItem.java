0781ebdd66981a8e6655d46a042e78b2a4e3bf5c
ill-education <72992133+ill-education@users.noreply.github.com>
2020-12-02 22:22:47 -0500

    Update MenuItem.java
    idk if im doing this right

--- MenuItem.java
+++ MenuItem.java
@@ -5,14 +5,112 @@
  */
 package restaurantsystem;
 import restaurantsystem.ByteCLI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
 /**
  *
- * @author AresUII
+ * @author ill-education
  */
 public class MenuItem {
-    byte[] itemName;
+    private byte[] itemName = new byte[50];
+    private double price;
+    private ArrayList<byte[]> customerOptions = new ArrayList<byte[]>();
+
+    public MenuItem() {
+    	byte[] a = "Item Name".getBytes();
+    	System.arraycopy(a,0,itemName,0,a.length);
+    	price = 0.00;
+	}
+
+    public MenuItem(byte[] name, double p) {
+    	System.arraycopy(name,0,itemName,0,name.length);
+    	price = p;
+	}
+
     //TODO: bools or bytes for any options we need to add
-    public void setName(byte[] a){
+    public void setMenuItemName(byte[] a){
         System.arraycopy(a,0,itemName,0,a.length);
+    }
+
+    public byte[] getMenuItemName(){
+        return itemName;
+    }
+
+    public void setMenuItemPrice(double p){
+        price = p;
+    }
+
+    public double getMenuItemPrice(){
+        return price;
+    }
+
+    public void addCustomerOption(byte[] o){
+
+    	boolean optionExists = false;
+
+    	for (int i = 0; i < customerOptions.size(); i++) {
+			if (Arrays.equals(customerOptions.get(i),o)){
+				optionExists = true;
+			}
+		}
+
+    	if (optionExists == false){
+
+    		customerOptions.add(o);
+
+    		ByteCLI.printBytes(o);
+    		System.out.print(" has been added to Customer Options");
+    		System.out.println();
+    	}
+    	else{
+    		ByteCLI.printBytes(o);
+    		System.out.print(" already exists in Customer Options");
+    		System.out.println();
+    	}
+    }
+
+    public void removeCustomerOption(byte[] o){
+
+    	boolean optionExists = false;
+    	int loc = -1;
+
+    	for (int i = 0; i < customerOptions.size(); i++) {
+			if (Arrays.equals(customerOptions.get(i),o)){
+				optionExists = true;
+				loc = i;
+			}
+		}
+
+    	if (optionExists){
+
+    		customerOptions.remove(loc);
+
+    		ByteCLI.printBytes(o);
+    		System.out.print(" has been removed from Customer Options");
+    		System.out.println();
+    	}
+    	else{
+    		ByteCLI.printBytes(o);
+    		System.out.print(" does not exist in Customer Options");
+    		System.out.println();
+    	}
+
+    }
+
+    public ArrayList<byte[]> getCustomerOptions(){
+        return customerOptions;
+    }
+
+    public void displayCustomerOptions(){
+
+    	ListIterator<byte[]> optionList = customerOptions.listIterator();
+
+    	while (optionList.hasNext()) {
+    		ByteCLI.printBytes(optionList.next());
+    		System.out.print(" ");
+    	}
+
+
     }
 }
